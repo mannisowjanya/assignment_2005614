67ef0bb44b9ec44c440f7bb0c21bc9d33191eaa8
Sowjanya_2005614 <55553331+mannisowjanya@users.noreply.github.com>
2020-06-05 18:33:28 +0530

    Update odd_even.c

--- odd_even.c
+++ odd_even.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 int main()
 {
-int a[10],i,num;
+int a[10],i,num,evencount=0,oddcount=0;
 printf("enter the size of the array: \n);
 scanf("%d",&num);
 printf("enter the elements of an array: \n);
@@ -16,8 +16,11 @@ for(i=0;i<num;i++)
 if(a[i]%2==0)
 {
 printf("%d\t",a[i]);
+evencount++;
 }
 }
+printf("\n number of even numbers in the array \n"); 
+printf("%d",evencount);
 printf("\n");
 printf("odd numbers in the array: \n");
 for(i=0;i<num;i++)
@@ -25,6 +28,9 @@ for(i=0;i<num;i++)
 if(a[i]%2!=0)
 {
 printf("%d\t",a[i]);
+oddcount++;
 }
 }
+printf("\n number of odd numbers in the array: \n);
+printf("%d",oddcount);
 }
